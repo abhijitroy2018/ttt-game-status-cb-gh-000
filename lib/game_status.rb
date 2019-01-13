042bea31ce34edbf24710765c7a52c92e0644513
Abhijit Roy <aroy30@uic.edu>
2019-01-13 20:00:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  
+
   WIN_COMBINATIONS.each do |win_combination|
     winning_positions_taken = position_taken?(board, win_combination[0]) &&
     position_taken?(board, win_combination[1]) &&
