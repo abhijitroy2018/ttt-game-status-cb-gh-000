7eaeac3481b10a770054ed6c206e16ce7f115c35
Abhijit Roy <aroy30@uic.edu>
2019-01-13 20:04:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,26 +24,27 @@ def won?(board)
       winning_positions_taken = position_taken?(board, win_combination[0]) &&
       position_taken?(board, win_combination[1]) &&
       position_taken?(board, win_combination[2])
-
+      
       if winning_positions_taken
         # All Xs
         all_Xs = (board[win_combination[0]] == "X" &&
         board[win_combination[1]] == "X" &&
         board[win_combination[2]] == "X"
         )
-
+        
         # All Os
         all_Os = (board[win_combination[0]] == "O" &&
         board[win_combination[1]] == "O" &&
         board[win_combination[2]] == "O"
         )
-
+        
         if all_Xs || all_Os
           return win_combination
         end
       end # end of outer if
     end # end of do-loop
-end # end of def won
+  end # end of def won
+end
 
 # def full?(board)
 #
