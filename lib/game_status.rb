0f8d5adaa1f4b12ffc00fab1be849d1a4cc7ecb7
Abhijit Roy <aroy30@uic.edu>
2019-01-13 19:07:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,9 +31,9 @@ def won?(board)
 
       if all_Xs || all_Os
         return win_combination
-      else
-        return winning_positions_taken
-      end
+      # else
+      #   return winning_positions_taken
+      # end
 
     end # end of outer if
   end # end of do-loop
