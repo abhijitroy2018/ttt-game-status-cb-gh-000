ea8c71031b0cbf637e1a300d7dc2c1d9defdecfb
Abhijit Roy <aroy30@uic.edu>
2019-01-13 21:54:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,7 +39,7 @@ def won?(board)
         )
 
         if all_Xs || all_Os
-          return win_combination.to_ary
+          return win_combination
         else
           # board_any = board.any? {|element| element == "X" || element=="O"}
           # if board_any
